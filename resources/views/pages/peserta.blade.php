50c7365751b32a335dece82ec994f43e113e5122
Rizki Giant S <ganx712@gmail.com>
2023-08-09 08:45:54 +0700

    first commit
    install laravel, route, slicing, dynamic menu/content

---
+++ resources/views/pages/peserta.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.content')
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>Dashboard Page</h1>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h4>Daftar Peserta Didik</h4>
+        </div>
+        <div class="card-body">
+            <p>Ini Isian Halaman</p>
+        </div>
+    </div>
+
+</section>
+@endsection
